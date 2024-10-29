20da413465e79f950ba51c09e0df06f4b340e957
PhongHiep08 <maiphonghieplp@gmail.com>
2024-10-29 19:39:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('yes-btn').addEventListener('click', function() {
+    document.getElementById('message').innerHTML = "tớ cũng iu cậu ♥!";
+});
+
+document.getElementById('no-btn').addEventListener('mouseover', function() {
+    let btn = document.getElementById('no-btn');
+    let randomX = Math.floor(Math.random() * (window.innerWidth - btn.clientWidth));
+    let randomY = Math.floor(Math.random() * (window.innerHeight - btn.clientHeight));
+
+    btn.style.position = 'absolute';
+    btn.style.left = randomX + 'px';
+    btn.style.top = randomY + 'px';
+});
